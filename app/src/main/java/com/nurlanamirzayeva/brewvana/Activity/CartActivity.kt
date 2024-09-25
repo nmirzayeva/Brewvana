8af9102641aebd40ddc9afc3b75274a335b5f723
Nurlana <lanamirzeyeva@gmail.com>
2024-09-26 00:41:44 +0400

    CartActivity.kt and activity_cart.xml added

---
+++ app/src/main/java/com/nurlanamirzayeva/brewvana/Activity/CartActivity.kt
@@ -0,0 +1,12 @@
+package com.nurlanamirzayeva.brewvana.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nurlanamirzayeva.brewvana.R
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
