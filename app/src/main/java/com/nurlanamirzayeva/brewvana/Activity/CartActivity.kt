d36c6836b83adbe18d573b1030b0cef94adafcb6
Nurlana <lanamirzeyeva@gmail.com>
2024-09-27 00:12:23 +0400

    viewholder_cart.xml created, fee calculation added to the CartActivity.kt

--- app/src/main/java/com/nurlanamirzayeva/brewvana/Activity/CartActivity.kt
+++ app/src/main/java/com/nurlanamirzayeva/brewvana/Activity/CartActivity.kt
@@ -2,11 +2,54 @@ package com.nurlanamirzayeva.brewvana.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.project1762.Helper.ManagmentCart
 import com.nurlanamirzayeva.brewvana.R
+import com.nurlanamirzayeva.brewvana.databinding.ActivityCartBinding
 
 class CartActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCartBinding
+    lateinit var managment: ManagmentCart
+    private var tax: Double = 0.0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cart)
+        binding = ActivityCartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        managment = ManagmentCart(this)
+
+        calculateCart()
+        setVariable()
+        initCartList()
     }
+
+    private fun initCartList() {
+        with(binding){
+            cartView.layoutManager=LinearLayoutManager(this@CartActivity,LinearLayoutManager.VERTICAL,false)
+        }
+    }
+
+    private fun setVariable() {
+        binding.backBtn.setOnClickListener { finish() }
+    }
+
+    private fun calculateCart() {
+        val percentTax = 0.02
+        val delivery = 15.0
+        tax = Math.round((managment.getTotalFee() * percentTax) * 100) / 100.0
+        val total = Math.round((managment.getTotalFee() + tax + delivery) * 100) / 100
+        val itemTotal = Math.round(managment.getTotalFee() * 100) / 100
+
+        with(binding) {
+            totalFeeTxt.text = "$$itemTotal"
+            taxTxt.text = "$$tax"
+            deliveryTxt.text = "$$delivery"
+            totalTxt.text = "$$total"
+        }
+
+    }
+
 }
